77ac54d4640d1398484c4a76369bc31f3f3883a2
Sreehari Prathap <sreehariprathap1996@gmail.com>
2021-07-10 03:01:18 +0530

    hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,35 +12,25 @@ import { useState, useEffect } from "react"
 //?IMAGE
 import NoImage from "../images/no_image.jpg"
 
+//? custom hook
+import { useHomeFetch } from "../hooks/useHomeFetch"
+//? HERO IMAGE
+import { HeroImage } from "./HeroImage"
+
 export const Home = () => {
-  const [state, setState] = useState("")
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false)
-      setLoading(true)
-
-      const movies = await API.fetchMovies(searchTerm, page)
-      // console.log(movies)
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }))
-    } catch (error) {
-      setError(true)
-    }
-    setLoading(false)
-  }
-
-  //? initial render
-  useEffect(() => {
-    fetchMovies(1)
-  }, [])
+  const { state, loading, error } = useHomeFetch()
 
   console.log(state)
 
-  return <div>Home page</div>
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  )
 }
